3208fc2a1564671f8b516d982d3d6aa6fc136a60
FlowerAccpeted <accepted_flower@outlook.com>
2026-01-18 10:24:55 +0800

    upd applier

--- apply.user.js
+++ apply.user.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name         Florrio Custom Language Uploader
+// @namespace    http://tampermonkey.net/
+// @description  Adds a button to upload custom language files in Florrio
+// @version      1.0
+// @author       FlowerAccepted
+// @match        *://*.florr.io/*
+// @grant        none
+// ==/UserScript==
 const btn = document.createElement('button');
 btn.textContent = 'Click me to Upload';
 btn.style.position = 'fixed';
